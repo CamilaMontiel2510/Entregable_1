d68beeba1a1eaea5b0a8aa76ad3efb06fd0594e3
CamilaMontiel <camila.montiel@udea.edu.co>
2024-03-13 20:24:39 -0500

    cambios: e añaden setters y getters de clase implantes

---
+++ GRUPO_CamilaMontielZapata_.py
@@ -0,0 +1,46 @@
+class Implantesb:
+    def __init__(self, id, fecha, mto, ):
+       self.__ID_implante = id
+       self.__fecha_ingreso = fecha 
+       self.__mantenimiento = mto
+       
+       def get_id(self):
+           return self.__ID_implante
+       def get_fecha(self):
+           return self.__fecha_ingreso
+       def get_mantenim(self):
+           return self.__manteimiento
+       def set_id(self, id):
+            self.__ID_implante = id
+       def set_fecha(self, fecha):
+           self.__fecha_ingreso = fecha
+       def set_mantenim(self, mto):
+           self.__manteimiento = mto
+       
+       
+       
+    
+class Prot_c:
+    def __init__(self) -> None:
+        pass
+class Marcapasos:
+    def __init__(self) -> None:
+        pass
+class Stent_c:
+    def __init__(self) -> None:
+        pass
+class Implante_d:
+    def __init__(self) -> None:
+        pass
+class Prot_rod:
+    def __init__(self) -> None:
+        pass
+    pass
+#interfaz de unsuario
+class Sistema:
+    def __init__(self) -> None:
+        pass 
+while True:
+    sistema_prot = Sistema()
+
+
